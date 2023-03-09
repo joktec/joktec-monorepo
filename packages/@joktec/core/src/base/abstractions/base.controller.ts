bec9630b09c7cb71ac5381a0cdd8e5e39adfa3da
Bao Tran <trangiabao1203@gmail.com>
2023-03-09 09:25:29 +0700

    refactor: clean docker from source

--- packages/@joktec/core/src/base/abstractions/base.controller.ts
+++ packages/@joktec/core/src/base/abstractions/base.controller.ts
@@ -1,53 +1,83 @@
 import { Body, Delete, Get, Param, Post, Put, Query, UseInterceptors } from '@nestjs/common';
-import { ApiForbiddenResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { BaseService } from './base.service';
 import { IBaseRequest, IListResponseDto } from '../models';
 import { QueryInterceptor, ResponseInterceptor } from './base.interceptor';
+import { toPlural, toSingular } from '../../utils';
+import { startCase } from 'lodash';
 
-@UseInterceptors(ResponseInterceptor)
-export abstract class BaseController<T, ID> {
-  protected constructor(protected service: BaseService<T, ID>) {}
-
-  @Get('/')
-  @ApiOperation({ summary: 'List' })
-  @ApiOkResponse()
-  @ApiForbiddenResponse()
-  @UseInterceptors(QueryInterceptor)
-  async findAll(@Query() req: IBaseRequest): Promise<IListResponseDto<T>> {
-    return this.service.findAll(req);
-  }
-
-  @Get('/:id')
-  @ApiOperation({ summary: 'Get' })
-  @ApiOkResponse()
-  @ApiNotFoundResponse()
-  async findOne(@Param('id') id: ID): Promise<T> {
-    return this.service.findOne(id);
-  }
-
-  @Post('/')
-  @ApiOperation({ summary: 'Create' })
-  @ApiOkResponse()
-  @ApiForbiddenResponse()
-  async create(@Body() entity: T): Promise<T> {
-    return this.service.create(entity);
-  }
-
-  @Put('/:id')
-  @ApiOperation({ summary: 'Update' })
-  @ApiOkResponse()
-  @ApiNotFoundResponse()
-  @ApiForbiddenResponse()
-  async update(@Param('id') id: ID, @Body() entity: T): Promise<T> {
-    return this.service.update(id, entity);
-  }
-
-  @Delete('/:id')
-  @ApiOperation({ summary: 'Delete' })
-  @ApiOkResponse()
-  @ApiNotFoundResponse()
-  @ApiForbiddenResponse()
-  async delete(@Param('id') id: ID): Promise<T> {
-    return this.service.delete(id);
-  }
+export interface IBaseControllerProps<T> {
+  dtoName: string;
+  dto: new (...args: any) => T;
+  apiTag?: string;
 }
+
+export const BaseController = <T, ID>(props: IBaseControllerProps<T>): any => {
+  const nameSingular = startCase(toSingular(props.dtoName));
+  const namePlural = toPlural(nameSingular);
+  const apiTag = props.apiTag || nameSingular;
+
+  @ApiTags(apiTag)
+  @UseInterceptors(ResponseInterceptor)
+  abstract class ApiController {
+    protected constructor(protected service: BaseService<T, ID>) {}
+
+    @Get('/')
+    @ApiOperation({ summary: `List ${namePlural}` })
+    @ApiOkResponse()
+    @ApiForbiddenResponse()
+    @UseInterceptors(QueryInterceptor)
+    async findAll(@Query() req: IBaseRequest): Promise<IListResponseDto<T>> {
+      return this.service.findAll(req);
+    }
+
+    @Get('/:id')
+    @ApiOperation({ summary: `Get ${nameSingular}` })
+    @ApiOkResponse({ type: props.dto })
+    @ApiNotFoundResponse()
+    @ApiParam({ name: 'id' })
+    async findOne(@Param('id') id: ID): Promise<T> {
+      return this.service.findOne(id);
+    }
+
+    @Post('/')
+    @ApiOperation({ summary: `Create ${nameSingular}` })
+    @ApiOkResponse({ type: props.dto })
+    @ApiForbiddenResponse()
+    @ApiBody({ type: props.dto })
+    async create(@Body() entity: T): Promise<T> {
+      return this.service.create(entity);
+    }
+
+    @Put('/:id')
+    @ApiOperation({ summary: `Update ${nameSingular}` })
+    @ApiOkResponse({ type: props.dto })
+    @ApiNotFoundResponse()
+    @ApiForbiddenResponse()
+    @ApiParam({ name: 'id' })
+    @ApiBody({ type: props.dto })
+    async update(@Param('id') id: ID, @Body() entity: T): Promise<T> {
+      return this.service.update(id, entity);
+    }
+
+    @Delete('/:id')
+    @ApiOperation({ summary: `Delete ${nameSingular}` })
+    @ApiOkResponse({ type: props.dto })
+    @ApiNotFoundResponse()
+    @ApiForbiddenResponse()
+    @ApiParam({ name: 'id' })
+    async delete(@Param('id') id: ID): Promise<T> {
+      return this.service.delete(id);
+    }
+  }
+
+  return ApiController;
+};
