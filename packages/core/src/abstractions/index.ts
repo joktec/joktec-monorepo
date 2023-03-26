1cd4ccbde765506013f9014200ae0e1009d57944
Bao Tran <trangiabao1203@gmail.com>
2023-03-26 12:21:34 +0700

    chore: dep upgrade

--- packages/core/src/abstractions/index.ts
+++ packages/core/src/abstractions/index.ts
@@ -1,6 +1,5 @@
 export * from './base.controller';
 export * from './base.dataloader';
-export * from './base.middleware';
 export * from './base.repository';
 export * from './base.resolver';
 export * from './base.service';
