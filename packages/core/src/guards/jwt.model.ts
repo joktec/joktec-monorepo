133581a5c76b1d52764e6f982206276fa9325e9d
Bao Tran <trangiabao1203@gmail.com>
2023-03-26 12:04:40 +0700

    fix: jwt model

--- packages/core/src/guards/jwt.model.ts
+++ packages/core/src/guards/jwt.model.ts
@@ -1,6 +1,14 @@
 import jwt from 'jsonwebtoken';
 
 export interface JwtPayload extends jwt.JwtPayload {
+  iss?: string | undefined;
+  sub?: string | undefined;
+  aud?: string | string[] | undefined;
+  exp?: number | undefined;
+  nbf?: number | undefined;
+  iat?: number | undefined;
+  jti?: string | undefined;
+
   userId: string;
   email?: string;
 }
