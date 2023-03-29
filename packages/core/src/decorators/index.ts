39e0435939ed3be46c841cea1acca48b25a015c6
Bao Tran <trangiabao1203@gmail.com>
2023-03-29 13:48:17 +0700

    impl: create decorator for default value of dto

--- packages/core/src/decorators/index.ts
+++ packages/core/src/decorators/index.ts
@@ -1,2 +1,9 @@
+import { Transform } from 'class-transformer';
+import { isNil } from 'lodash';
+
 export * from './base-class.decorator';
 export * from './base-method.decorator';
+
+export const Default = (defaultValue: any): PropertyDecorator => {
+  return Transform(({ value }) => (isNil(value) ? defaultValue : value));
+};
