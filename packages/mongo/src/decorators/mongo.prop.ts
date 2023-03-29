354d67759b04cd53e68c7c9851424ea98c1a36b9
Bao Tran <trangiabao1203@gmail.com>
2023-03-29 13:11:12 +0700

    impl: update mongo prop url

--- packages/mongo/src/decorators/mongo.prop.ts
+++ packages/mongo/src/decorators/mongo.prop.ts
@@ -1,18 +1,21 @@
 import { linkTransform } from '@joktec/core';
-import { prop } from '@typegoose/typegoose';
+import { prop, PropType } from '@typegoose/typegoose';
 import { BasePropOptions } from '@typegoose/typegoose/lib/types';
-import { PropType } from '@typegoose/typegoose/lib/internal/constants';
 
 export const propUrl = (options?: BasePropOptions & { host?: string }, kind?: PropType): PropertyDecorator => {
   const host = options?.host || process.env.MISC_CDN_URL || '';
-  return prop(
-    {
-      default: null,
-      type: String,
-      get: (value: string) => linkTransform(value, host, 'relative'),
-      set: (value: string) => linkTransform(value, host, 'absolute'),
-      ...options,
-    },
-    kind,
-  );
+  const isArray: boolean = kind === PropType.ARRAY;
+  const getter = (value: string | string[]): string | string[] => {
+    if (Array.isArray(value)) {
+      return value.map(v => linkTransform(v, host, 'relative'));
+    }
+    return linkTransform(value, host, 'relative');
+  };
+  const setter = (value: string | string[]): string | string[] => {
+    if (Array.isArray(value)) {
+      return value.map(v => linkTransform(v, host, 'absolute'));
+    }
+    return linkTransform(value, host, 'absolute');
+  };
+  return prop({ default: isArray ? [] : null, type: String, get: getter, set: setter, ...options }, kind);
 };
