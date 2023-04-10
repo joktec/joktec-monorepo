12227a38d4c9d149d48518e14760444105487f73
Bao Tran <trangiabao1203@gmail.com>
2023-04-10 18:05:19 +0700

    chore: remove api property mongo schema

--- packages/mongo/src/models/mongo.schema.ts
+++ packages/mongo/src/models/mongo.schema.ts
@@ -1,38 +1,32 @@
 import { mongoose, plugin, prop } from '@typegoose/typegoose';
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
-import { ApiProperty, Field } from '@joktec/core';
+import { Field } from '@joktec/core';
 
 export interface MongoSchema extends Base {}
 
 @plugin(require('mongoose-beautiful-unique-validation'), { defaultMessage: '{PATH}_MUST_BE_UNIQUE' })
 export abstract class MongoSchema extends TimeStamps {
   @prop({ type: Date, default: new Date(), immutable: true })
-  @ApiProperty({ type: Date })
   @Field(() => Date, { nullable: true })
   createdAt?: Date;
 
   @prop({ type: mongoose.Types.ObjectId, default: null, immutable: true })
-  @ApiProperty({ type: String, example: '507f1f77bcf86cd799439011' })
   @Field(() => String, { nullable: true })
   createdBy?: mongoose.Types.ObjectId;
 
   @prop({ type: Date, default: new Date() })
-  @ApiProperty({ type: Date })
   @Field(() => Date, { nullable: true })
   updatedAt?: Date;
 
   @prop({ type: mongoose.Types.ObjectId, default: null })
-  @ApiProperty({ type: String, example: '507f1f77bcf86cd799439011' })
   @Field(() => String, { nullable: true })
   updatedBy?: mongoose.Types.ObjectId;
 
   @prop({ type: Date, default: null })
-  @ApiProperty({ type: Date })
   @Field(() => Date, { nullable: true })
   deletedAt?: Date;
 
   @prop({ type: mongoose.Types.ObjectId, default: null })
-  @ApiProperty({ type: String, example: '507f1f77bcf86cd799439011' })
   @Field(() => String, { nullable: true })
   deletedBy?: mongoose.Types.ObjectId;
 }
