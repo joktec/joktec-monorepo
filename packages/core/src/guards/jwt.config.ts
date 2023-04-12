600a53b200364c207b63cb70261eb738f29fae44
Bao Tran <trangiabao1203@gmail.com>
2023-04-12 09:42:02 +0700

    impl: remove jwt fields config

--- packages/core/src/guards/jwt.config.ts
+++ packages/core/src/guards/jwt.config.ts
@@ -1,21 +1,15 @@
-import { toInt } from '../utils';
-
 export enum JwtContext {
   HTTP,
   GQL,
 }
 
 export class JwtConfig {
-  pending: number;
-  limit: number;
   secretKey: string;
   refreshKey: string;
   expired: string;
 
   constructor(props: Partial<JwtConfig> = {}) {
     Object.assign(this, {
-      pending: toInt(props.pending, 30),
-      limit: toInt(props.limit, 4),
       secretKey: props.secretKey || '$ecr3t_4ey',
       refreshKey: props.refreshKey || 'r3fre$h_4ey',
       expired: props.expired || '30 days',
