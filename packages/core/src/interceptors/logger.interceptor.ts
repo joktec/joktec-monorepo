71c4f89ccd4e3e22e467157ce83d79ab5db12505
Bao Tran <trangiabao1203@gmail.com>
2023-05-30 15:16:01 +0700

    impl: logger handle exception

--- packages/core/src/interceptors/logger.interceptor.ts
+++ packages/core/src/interceptors/logger.interceptor.ts
@@ -1,7 +1,6 @@
 import { Reflector } from '@nestjs/core';
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { RENDER_METADATA } from '@nestjs/common/constants';
-import { Observable } from 'rxjs';
+import { CallHandler, ExecutionContext, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common';
+import { catchError, Observable, throwError } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Request } from 'express';
 import { LogService } from '../log';
@@ -15,24 +14,40 @@ export class LoggerInterceptor implements NestInterceptor {
     const start = Date.now();
     return next.handle().pipe(
       tap(() => {
-        const end = Date.now();
-        const elapsedTime = end - start;
-        let timeString = `${elapsedTime} ms`;
-        if (elapsedTime >= 1500) {
-          timeString = `${(elapsedTime / 1000).toFixed(2)} s`;
-        }
-
+        const timeString = this.getTimeString(start);
         const method = req.method.toUpperCase();
         const path = req.originalUrl;
         const status = context.switchToHttp().getResponse().statusCode;
-
-        const renderView = this.reflector.get<string>(RENDER_METADATA, context.getHandler());
-        if (renderView) {
-          this.logger.info('http: %s - %s (%s) %s - View: %s', method, path, timeString, status, renderView);
-        } else {
+        if (status >= HttpStatus.OK && status < HttpStatus.BAD_REQUEST) {
           this.logger.info('http: %s - %s (%s) %s', method, path, timeString, status);
+        } else {
+          this.logger.warn('http: %s - %s (%s) %s', method, path, timeString, status);
         }
       }),
+      catchError(error => {
+        const timeString = this.getTimeString(start);
+        const method = req.method.toUpperCase();
+        const path = req.originalUrl;
+        const status = context.switchToHttp().getResponse().statusCode || HttpStatus.INTERNAL_SERVER_ERROR;
+
+        if (status >= HttpStatus.BAD_REQUEST && status < HttpStatus.INTERNAL_SERVER_ERROR) {
+          this.logger.warn('http: %s - %s (%s) %s', method, path, timeString, status);
+        } else {
+          this.logger.error('http: %s - %s (%s) %s', method, path, timeString, status);
+        }
+
+        return throwError(error);
+      }),
     );
+  }
+
+  private getTimeString(start: number): string {
+    const end = Date.now();
+    const elapsedTime = end - start;
+    let timeString = `${elapsedTime} ms`;
+    if (elapsedTime >= 1500) {
+      timeString = `${(elapsedTime / 1000).toFixed(2)} s`;
+    }
+    return timeString;
   }
 }
