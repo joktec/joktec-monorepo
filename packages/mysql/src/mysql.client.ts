49f005ae764d82b53eca8aa16f455d7f71cf9251
Bao Tran <trangiabao1203@gmail.com>
2023-11-20 09:17:55 +0700

    impl: mysql export erd

--- packages/mysql/src/mysql.client.ts
+++ packages/mysql/src/mysql.client.ts
@@ -2,7 +2,9 @@ import { IBaseRepository, Client, DeepPartial, ICondition } from '@joktec/core';
 import { Model, Sequelize } from 'sequelize-typescript';
 import { MysqlConfig } from './mysql.config';
 
-export interface MysqlClient extends Client<MysqlConfig, Sequelize> {}
+export interface MysqlClient extends Client<MysqlConfig, Sequelize> {
+  exportDiagram(conId?: string): Promise<void>;
+}
 
 export interface IMysqlRepository<T extends Model<T>, ID> extends IBaseRepository<T, ID> {
   upsert(condition: ICondition, body: DeepPartial<T>): Promise<T>;
