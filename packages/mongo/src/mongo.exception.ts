bca567444d4edd19e55e08be44748fd44fcff1d4
Joker <trangiabao1203@gmail.com>
2023-12-04 13:16:30 +0700

    impl: handle unique exception

--- packages/mongo/src/mongo.exception.ts
+++ packages/mongo/src/mongo.exception.ts
@@ -6,6 +6,7 @@ import {
   ValidateException,
 } from '@joktec/core';
 import { Error } from 'mongoose';
+import { has, isEmpty, upperCase } from 'lodash';
 
 export class MongoException extends InternalServerException {
   constructor(msg: string = 'MONGO_EXCEPTION', error: any) {
@@ -18,21 +19,30 @@ export const MongoCatch = BaseMethodDecorator(async (options: CallbackMethodOpti
   try {
     return await method(...args);
   } catch (err) {
-    if (err instanceof Error.ValidationError) {
-      const formatError: IValidateError = {};
-      Object.values(err.errors).map(errItem => {
-        if (!formatError.hasOwnProperty(errItem.path)) {
-          formatError[errItem.path] = [];
-        }
-        const message: string =
-          errItem instanceof Error.CastError ? `${errItem.path}_INVALID`.toUpperCase() : errItem.message;
-        formatError[errItem.path].push(message);
-      });
-      throw new ValidateException(formatError);
+    const formatError: IValidateError = {};
+
+    function injectError(path: string, errMsgCode: string) {
+      if (!formatError[path]) formatError[path] = [];
+      formatError[path].push(errMsgCode);
     }
 
-    if (err?.code === 11000 || err?.code === 11001) {
-      // Skip
+    if (err instanceof Error.ValidationError) {
+      Object.values(err.errors).map(errItem => {
+        const message: string =
+          errItem instanceof Error.CastError ? `${errItem.path}_INVALID`.toUpperCase() : errItem.message;
+        injectError(errItem.path, message);
+      });
+    }
+
+    // Handle unique error
+    if ((err?.code === 11000 || err?.code === 11001) && has(err, 'keyValue')) {
+      Object.keys(err['keyValue']).map(path => {
+        injectError(path, `${upperCase(path)}_DUPLICATED_VALUE`);
+      });
+    }
+
+    if (!isEmpty(formatError)) {
+      throw new ValidateException(formatError);
     }
 
     throw new MongoException(err.message, err);
