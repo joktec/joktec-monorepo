2175141d60f2f1d338aaf14f3dc6740f9531ed04
Bao Tran <trangiabao1203@gmail.com>
2024-09-20 14:14:52 +0700

    impl: enhance http exception

--- packages/http/src/http.exception.ts
+++ packages/http/src/http.exception.ts
@@ -1,8 +1,17 @@
 import { Exception, HttpStatus, InternalServerException } from '@joktec/core';
-import { AxiosError } from 'axios';
+import { AxiosError, AxiosResponseHeaders, InternalAxiosRequestConfig, RawAxiosResponseHeaders } from 'axios';
 
-export class CustomHttpException extends InternalServerException {
-  constructor(message: string, data: any = null) {
+export interface IHttpException {
+  data?: any;
+  status?: number;
+  statusText?: string;
+  headers?: RawAxiosResponseHeaders | AxiosResponseHeaders;
+  config?: InternalAxiosRequestConfig<any>;
+  request?: any;
+}
+
+export class HttpClientException extends InternalServerException<IHttpException> {
+  constructor(message: string, data: IHttpException = null) {
     super(message, data);
   }
 }
@@ -12,12 +21,12 @@ export function httpExceptionHandler(err: AxiosError): Exception {
   if (err?.response) {
     const status: number = err.response?.status;
     if (Object.values(HttpStatus).includes(status)) {
-      return new CustomHttpException(err.response.statusText, err.response);
+      return new HttpClientException(err.response.statusText, err.response);
     }
   }
 
   // The request was made but no response was received `error.request`
   // is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in node.js
   // [OR] Something happened in setting up the request that triggered an Error
-  return new CustomHttpException(err.message ?? 'Unknown', err?.request || err);
+  return new HttpClientException(err.message ?? 'Unknown', { request: err?.request });
 }
--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -1,4 +1,5 @@
 export * from './models';
+export * from './http.exception';
 export * from './http.config';
 export * from './http.client';
 export * from './http.metric';
