15121bc2182efd9524b3b232cbefbdeb07e55e04
Brian Tran <trangiabao1203@gmail.com>
2024-11-08 20:48:58 +0700

    fix: job worker

--- packages/cron/src/jobs/job.worker.ts
+++ packages/cron/src/jobs/job.worker.ts
@@ -80,7 +80,7 @@ export abstract class JobWorker<
     const updatedJobs = newJobs.map(newJob => {
       const currentJob = currentJobs.find(currJob => currJob.code === newJob.code);
       if (currentJob && !this.config.startFromScratch) {
-        merge(newJob, currentJob);
+        return merge({}, newJob, currentJob);
       }
       return newJob;
     });
