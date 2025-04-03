57f9bc40c2394fbb66c1cbf3b8950eeaee533f83
Brian Tran <trangiabao1203@gmail.com>
2025-04-03 14:39:17 +0700

    impl: rabbit flattern messages

--- packages/brokers/rabbit/src/loaders/rabbit.publisher.ts
+++ packages/brokers/rabbit/src/loaders/rabbit.publisher.ts
@@ -1,6 +1,7 @@
 import { BaseMethodDecorator, CallbackMethodOptions, DEFAULT_CON_ID } from '@joktec/core';
 import { RabbitPublishOptions } from '../models';
 import { RabbitService } from '../rabbit.service';
+import { toArray } from '@joktec/utils';
 
 export function RabbitPublish(queue: string, options?: RabbitPublishOptions, conId?: string): MethodDecorator;
 
@@ -48,9 +49,9 @@ export function RabbitPublish<T = any>(
 
       try {
         const result: T = await method(...args);
-        if (!result) return null;
+        if (!result || (Array.isArray(result) && !result.length)) return result;
 
-        const messages = Array.isArray(result) ? result : [result];
+        const messages = toArray(result);
 
         if (queue) {
           const msgArray = messages.map((msg: T) => JSON.stringify(msg));
